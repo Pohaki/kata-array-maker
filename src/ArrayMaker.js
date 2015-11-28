c7841b226c8e019effa2282f0e5823f490216340
Pohaki <kevin.lachaise@gmail.com>
2015-11-28 19:48:38 +0100

    Exo finis

--- src/ArrayMaker.js
+++ src/ArrayMaker.js
@@ -1,19 +1,14 @@
   var tab = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-  var tab2 = new Array();
 function arrayMaker(max) {
   if(isNaN(max)) {
     return null;
   } else if(max === 0) {
     return [];
-  }else if (max > 1){
-      for(var i = 0; i < 1; i++){
-        if (tab[i] === 1){
-          for (var j = 0; j < max; j++){
-            tab2.push(tab[j]);
+  }else{
+    var tab2 = []
+      for(var i = 1; i <= max; i++){
+            tab2.push(i);
+            }
+            return tab2;
           }
-        }
-        console.log(tab2);
-        return tab2;
-      }
-    }
 };
